d2a81cb9dc3361bdf4fac5fee3482da7e7b9a8a7
Kenny2006-PE <kenny.palacin@tecsup.edu.pe>
2025-06-30 12:31:07 -0500

    cambios

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const db = new Sequelize('bd_farmacia', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
   logging: false
 });
 db.authenticate()
