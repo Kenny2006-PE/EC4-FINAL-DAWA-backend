8dd1cb28320a34c78f24e881004d23d7d39d3259
Kenny2006-PE <kenny.palacin@tecsup.edu.pe>
2025-06-30 12:38:58 -0500

    Prueba 2

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
+
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  dialect: 'mysql', // o puedes usar process.env.DB_DIALECT si lo defines en .env
   host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
   logging: false
 });
+
 db.authenticate()
   .then(() => {
     console.log('✅ Conexión a MySQL exitosa');
